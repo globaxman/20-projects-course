14744a073c86c6d098d4c3571db9ce95d81cf489
Mikołaj Machnik <mikolaj.machnik93@gmail.com>
2023-07-06 19:58:20 +0200

    prepering to change quotes on the website

--- -- quotes generator/script.js
+++ -- quotes generator/script.js
@@ -1,5 +1,12 @@
 // Get quotes form API
 
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
 let apiQuotes = [];
 
 // show new quote
