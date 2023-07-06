20143bfd96b2f86d03ec734341cdec54f7992d1b
Mikołaj Machnik <mikolaj.machnik93@gmail.com>
2023-07-06 20:16:00 +0200

    changing font sizes depending on length

--- -- quotes generator/script.js
+++ -- quotes generator/script.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 // Get quotes form API
 
 const quoteContainer = document.getElementById('quote-container');
@@ -13,7 +14,21 @@ let apiQuotes = [];
 function newQuote(){
     // pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    authorText.textContent = quote.author;
+    
+    // check if author fiels is blank and replace it with 'unknown'
+    if(!quote.author){
+        authorText.textContent = 'Unknown';
+    }else {
+        authorText.textContent = quote.author;
+    }
+
+    // check quote length to determine styling
+    if(quote.text.length > 100){
+        quoteText.classList.add('long-quote')
+    } else {
+        quoteText.classList.remove('long-quote')
+    }
+    
     quoteText.textContent = quote.text;
 }
 
