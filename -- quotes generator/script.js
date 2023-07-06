3f78487c0922cff8b94e3ab7c33d7decc674c048
Mikołaj Machnik <mikolaj.machnik93@gmail.com>
2023-07-06 19:26:18 +0200

    get quotes from api provided

--- -- quotes generator/script.js
+++ -- quotes generator/script.js
@@ -1,1 +1,17 @@
-const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+// Get quotes form API
+
+let apiQuotes = [];
+
+async function getQuotes(){
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try {
+        const response  = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        console.log(apiQuotes[3]);
+    } catch(error) {
+        // Cattch Error here
+    }
+};
+
+// On load
+getQuotes();
