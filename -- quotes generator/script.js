28d1445ab57bd67d4a5ebd2264c59e1509ec690c
Mikołaj Machnik <mikolaj.machnik93@gmail.com>
2023-07-06 19:48:23 +0200

    get random quote from api

--- -- quotes generator/script.js
+++ -- quotes generator/script.js
@@ -2,12 +2,19 @@
 
 let apiQuotes = [];
 
+// show new quote
+function newQuote(){
+    // pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
 async function getQuotes(){
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response  = await fetch(apiUrl);
         apiQuotes = await response.json();
-        console.log(apiQuotes[3]);
+        newQuote();
     } catch(error) {
         // Cattch Error here
     }
